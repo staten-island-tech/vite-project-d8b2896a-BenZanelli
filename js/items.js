19ae1a011192d1e8081111ee9737994748b2cb2c
delusional51 <BenjaminZanelli51@gmail.com>
2023-11-08 11:26:05 -0500

    zxcZXC

---
+++ js/items.js
@@ -0,0 +1,27 @@
+const items = [
+    {
+        name: "jacket",
+        image: "https://allstar.de/thumbnail/40/07/e1/1669622454/9500H_3_1920x1920.jpg",
+        price: 299,
+        sale: true,
+        type: ["uniform", "fie"],    
+    },
+    {
+        name: "pants",
+        image: "https://allstar.de/thumbnail/05/ea/4e/1669622210/9501H_3_1920x1920.jpg",
+        price: 205,
+        sale: false,
+        type: ["uniform", "fie"],
+    },
+    {
+        name:"plastron",
+        image:"https://allstar.de/thumbnail/80/51/ac/1670251982/SPG-H_2_1920x1920.jpg",
+        price: 84,
+        sale: false,
+        type: ["uniform", "protection", "fie"]
+    }
+
+]
+
+
+export {items};
---
+++ js/main.js
@@ -0,0 +1,4 @@
+import "../styles/style.css";
+import { items } from "./items";
+
+console.log(items)
